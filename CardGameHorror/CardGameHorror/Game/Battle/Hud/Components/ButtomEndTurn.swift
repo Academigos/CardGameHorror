2a4644d47ed3019165a811990829caed69ecefa6
Erick <erickrocha965@gmail.com>
2023-07-20 17:14:12 -0300

    labels da vida e seleção de cartas

--- CardGameHorror/CardGameHorror/Game/Battle/Hud/Components/ButtomEndTurn.swift
+++ CardGameHorror/CardGameHorror/Game/Battle/Hud/Components/ButtomEndTurn.swift
@@ -10,11 +10,16 @@ import SpriteKit
 class ButtonEndTurn: SKSpriteNode{
     
     weak var endTurnButtonDelegate: endTurnDelegate?
+    var valueLabel: SKLabelNode = SKLabelNode()
+    
+    var counter = 0
+    let totalCount = 3
     
     let buttomTexture: SKTexture
     init(buttomTexture: String) {
         self.buttomTexture = SKTexture(imageNamed: buttomTexture)
         super.init(texture: self.buttomTexture, color: .clear, size: self.buttomTexture.size())
+        setupValueLabel()
         isUserInteractionEnabled = true
         if GameController.shared.selectedCard.count < 3 {
             alpha = 0.5
@@ -47,5 +52,21 @@ class ButtonEndTurn: SKSpriteNode{
         } else {
             alpha = 1.0
         }
+    }
+    private func setupValueLabel() {
+        valueLabel.name = "valueLabel"
+        valueLabel.fontSize = 12
+        valueLabel.fontName = "BigshotOne-Regular"
+        valueLabel.fontColor = .white
+        valueLabel.text = "\(counter)/\(totalCount)"
+        valueLabel.position = CGPoint(x: size.width * -0.2, y: size.height * 0.3)
+       // valueLabel.zPosition = 1.0
+        
+        addChild(valueLabel)
+    }
+    
+    func updtateValueCartas(){
+        counter = GameController.shared.selectedCard.count
+        valueLabel.text = "\(counter)/\(totalCount)"
     }
 }

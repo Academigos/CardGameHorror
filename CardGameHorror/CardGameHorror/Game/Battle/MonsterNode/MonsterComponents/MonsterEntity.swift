d2682aadc3db9585dbcdfec84b7d3419effd7bfd
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2023-07-21 17:45:05 -0300

    animation de ataque

--- CardGameHorror/CardGameHorror/Game/Battle/MonsterNode/MonsterComponents/MonsterEntity.swift
+++ CardGameHorror/CardGameHorror/Game/Battle/MonsterNode/MonsterComponents/MonsterEntity.swift
@@ -8,9 +8,9 @@
 import SpriteKit
 
 class Enemy: SKSpriteNode {
-    let enemyTexture:SKTexture
+    private let enemyTexture:SKTexture
     // Adicione mais texturas aqui para os outros quadros da animação
-    let idleTexture:[SKTexture] = [
+    private let idleTexture:[SKTexture] = [
         SKTexture(imageNamed: "bebadoIdle1"),
         SKTexture(imageNamed: "bebadoIdle2"),
         SKTexture(imageNamed: "bebadoIdle3"),
@@ -23,6 +23,13 @@ class Enemy: SKSpriteNode {
         SKTexture(imageNamed: "bebadoIdle10"),
         SKTexture(imageNamed: "bebadoIdle11"),
         // Adicione mais texturas aqui para os outros quadros da animação
+    ]
+    private let attackTexture:[SKTexture] = [
+        SKTexture(imageNamed: "ataque1"),
+        SKTexture(imageNamed: "ataque2"),
+        SKTexture(imageNamed: "ataque3"),
+        SKTexture(imageNamed: "ataque4"),
+        SKTexture(imageNamed: "ataque5")
     ]
     //let enemyFlavorTexr: SKLabelNode
     
@@ -37,11 +44,25 @@ class Enemy: SKSpriteNode {
     }
     
     func attacking() {
-        self.removeAllActions()
-        //        let frameChanges
-        // mudança na tela
-        // Fala do inimigo
-        idle()
+        // Ação de animação de ataque
+        let attackAction = SKAction.animate(with: attackTexture, timePerFrame: 0.1)
+        
+        let original:CGSize = self.size
+        
+        // Ação de escala para aumentar o tamanho do nó (usando o SKAction.scale(by:))
+        let scaleUpAction = SKAction.scale(by: 1.7, duration: 0.5)
+        // Ação de escala para restaurar o tamanho original do nó
+        let scaleDownAction = SKAction.scale(to: original, duration: 0.3)
+        
+        // Sequência de ações para aumentar e restaurar o tamanho do nó durante o ataque
+        let scaleSequence = SKAction.sequence([scaleUpAction, attackAction, SKAction.wait(forDuration: 0.5), scaleDownAction])
+        
+        // Sequência de ações completa: animação de ataque + animação idle
+        let sequenceAction = SKAction.sequence([scaleSequence, SKAction.run { [weak self] in
+            self?.idle()
+        }])
+        
+        self.run(sequenceAction)
     }
     
     func takingDamage() {
@@ -50,9 +71,13 @@ class Enemy: SKSpriteNode {
             SKAction.scale(to: autoScale(self, widthProportion: 0.16, screenSize: GameViewController.screenSize), duration: 0.1),
             SKAction.scale(to: autoScale(self, widthProportion: 0.14, screenSize: GameViewController.screenSize), duration: 0.1),
             SKAction.scale(to: autoScale(self, widthProportion: 0.16, screenSize: GameViewController.screenSize), duration: 0.1),
-            SKAction.scale(to: autoScale(self, widthProportion: 0.14, screenSize: GameViewController.screenSize), duration: 0.1)
+            SKAction.scale(to: autoScale(self, widthProportion: 0.14, screenSize: GameViewController.screenSize), duration: 0.1),
+            SKAction.wait(forDuration: 1.0),
+            SKAction.run { [weak self] in
+                self?.attacking()
+            }
         ])
-
+        
         self.run(shakeAction)
         idle()
     }

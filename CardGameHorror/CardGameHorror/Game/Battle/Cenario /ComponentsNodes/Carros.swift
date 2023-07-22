4820e8061731b1f5ae6547106a20352a720920b2
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-07-22 19:20:07 -0300

    take out animation of farol (obs. we don't delete it we comment it)

--- CardGameHorror/CardGameHorror/Game/Battle/Cenario /ComponentsNodes/Carros.swift
+++ CardGameHorror/CardGameHorror/Game/Battle/Cenario /ComponentsNodes/Carros.swift
@@ -11,23 +11,23 @@ import GameplayKit
 class Carros: SKNode{
     let carro1  = Carro1(carro1: "Carro1")
     let carro2  = Carro2(carro2: "Carro2")
-    let farol = Farol()
+//    let farol = Farol()
     var fumaca:SKEmitterNode! = SKEmitterNode(fileNamed: "Fumaca")
     
     override init() {
         super.init()
         carro1.zPosition = 0
         carro2.zPosition = 1
-        farol.zPosition = 2
+//        farol.zPosition = 2
         fumaca.zPosition = 2
         
-        farol.position = CGPoint(x: 0, y: 0)
+//        farol.position = CGPoint(x: 0, y: 0)
 //        carro1.scale(to: autoScale(carro1, widthProportion: 1, screenSize: GameViewController.screenSize))
         //farol.scale(to: autoScale(farol, widthProportion: , screenSize: GameViewController.screenSize))
 //        carro2.scale(to: autoScale(carro2, widthProportion: 1, screenSize: GameViewController.screenSize))
         addChild(carro1)
         addChild(carro2)
-        addChild(farol)
+//        addChild(farol)
         criarFumaca()
     }
     

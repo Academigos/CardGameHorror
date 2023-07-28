f14e6ac20739ce84fe879c73434cc0f0ac7aa5c5
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-07-28 19:59:37 -0300

    endScene

--- CardGameHorror/CardGameHorror/Game/EndScene/PlayerEndScene.swift
+++ CardGameHorror/CardGameHorror/Game/EndScene/PlayerEndScene.swift
@@ -9,13 +9,37 @@ import Foundation
 import SpriteKit
 
 class PlayerEnd: SKScene{
-    let resetButtom = ResetButtom()
-    let backToMenu = MainMenuButtom()
+    let kaylaWakingVictory = KaylaWaking(imagem0: "prologo_vitoria_1", imagens: "prologo_vitoria_", pasta: "KaylaVitoria")// animacao
+    let finalScene = FinalSceneVitoria()//cena final (background e label)
+    
     override func didMove(to view: SKView) {
-        resetButtom.position = CGPoint(x: GameViewController.screenSize.width * 0.3 , y: GameViewController.screenSize.height * 0.5)
-        addChild(resetButtom)
+        kaylaWakingVictory.position = CGPoint(x: GameViewController.screenSize.width*0.5, y: GameViewController.screenSize.height*0.5)
+        finalScene.zPosition=0
+        let delay = SKAction.wait(forDuration: 3)
+        let fadeout = SKAction.sequence([delay,.fadeOut(withDuration: 1)])
+        addChild(kaylaWakingVictory)
+        kaylaWakingVictory.run(fadeout)
+        finalScene(resultado: finalScene)
         
-        backToMenu.position = CGPoint(x: GameViewController.screenSize.width * 0.7 , y: GameViewController.screenSize.height * 0.5)
-        addChild(backToMenu)
+        //addChild(finalScene)
+    }
+    
+    
+    //delay de 5 segundos
+    func finalScene(resultado:SKNode){
+        let delayTime: TimeInterval = 4.0
+               Timer.scheduledTimer(withTimeInterval: delayTime, repeats: false) { timer in
+                   let delay = SKAction.wait(forDuration: 0)
+                   let transicao = SKAction.sequence([SKAction.fadeAlpha(to: 0, duration: 0),delay, SKAction.fadeAlpha(to: 1.0, duration: 3)])
+                   
+                   self.removeAllChildren()
+                   self.addChild(resultado)
+                   resultado.run(transicao)
+               }
+        
+    }
+    
+    func addDialogo(){
+        
     }
 }
